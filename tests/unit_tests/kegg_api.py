e5b8a8308d2ab652d850bd9c97dcc81572a8ab3f
João Capela <pg38274@alunos.uminho.pt>
2023-04-17 16:33:18 +0100

    [ADD] kegg api tests and ci

---
+++ tests/unit_tests/kegg_api.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+import pandas as pd
+
+from sm_precursor_predictor.data_integration.kegg_api import KeggApi
+
+
+class TestKeggApi(TestCase):
+
+    def test_get_list(self):
+        list_of_pathways = KeggApi.get_list("pathway")
+        self.assertEqual(list_of_pathways.shape, (563, 2))
+
+    def test_get_info(self):
+        list_of_pathways = KeggApi.get_list("brite")
+        self.assertEqual(list_of_pathways.shape, (141, 2))
+        self.assertIn("br08011", list_of_pathways[0].values)
+
+    def test_get_secondary_metabolites(self):
+        secondary_metabolites = pd.read_html("https://www.genome.jp/kegg/compound/br08011.html")
+
+        self.assertEqual(secondary_metabolites[0].shape, (152, 6))
