c18f7cb7e6a71757a9bdd31215e9c18fc176a39e
Joao Cheixo <jonas4777@gmail.com>
2023-06-06 18:01:29 +0100

    [UPDATE] generate_sdf ,get_precursors_for_compound

--- src/sm_precursor_predictor/data_integration/kegg_get_all_for_compound.py
+++ src/sm_precursor_predictor/data_integration/kegg_get_all_for_compound.py
@@ -20,7 +20,8 @@ class KEGGAllCompoundInfoExtractor:
                 graph = graphs.get(map_id)
                 kegg_precursors_finder.graph = graph
                 compounds = kegg_precursors_finder.get_all_compounds_of_pathway()
-                precursor_compound_dict[list(precursors)[0]] = compounds
+                precursor_compound_dict[list(precursors)[0]] = compounds[1:]
+
             else:
                 for precursor in precursors:
                     precursor_compound_dict.setdefault(precursor, [])
@@ -54,26 +55,36 @@ class KEGGAllCompoundInfoExtractor:
         compound_prec_dict = {}
 
         for map_id in map_ids:
+
             kegg_precursors_finder = KEGGPrecursorFinder(map_id, data, create_graph=False)
-            kegg_precursors_finder.graph = graphs.get(map_id)
-            compounds = kegg_precursors_finder.get_all_compounds_of_pathway()
+            compounds = KEGGPrecursorFinder.get_all_compounds_of_pathway(graphs[map_id])
+            precursors = KEGGPrecursorFinder.get_precursors_in_pathway(map_id, data)
 
-            precursors = kegg_precursors_finder.get_precursors_in_pathway()
+            if len(precursors) == 1:
+                graph = graphs.get(map_id)
+                kegg_precursors_finder.graph = graph
 
-            for compound in compounds:
-                if compound not in precursors:
-                    compound_prec_dict.setdefault(compound, [])
+                for compound in compounds:
 
-                    for precursor in precursors:
-                        try:
-                            if precursor in graphs[map_id] and compound in graphs[map_id]:
-                                path = KEGGPrecursorFinder.find_path_from_source_to_target(graphs[map_id], precursor,
-                                                                                           compound)
-                                if path:
-                                    compound_prec_dict[compound] = list(
-                                        set(compound_prec_dict[compound]) | {precursor})
-                        except (nx.NodeNotFound, nx.NetworkXNoPath):
-                            pass
+                    compounds = kegg_precursors_finder.get_all_compounds_of_pathway()
+                    compound_prec_dict[compound] = [list(precursors)[0]]
+                    compound_prec_dict = {compound: precursors for compound, precursors in compound_prec_dict.items() if compound not in precursors}
+
+            else:
+                for compound in compounds:
+                    if compound not in precursors:
+                        compound_prec_dict.setdefault(compound, [])
+
+                        for precursor in precursors:
+                            try:
+                                if precursor in graphs[map_id] and compound in graphs[map_id]:
+                                    path = KEGGPrecursorFinder.find_path_from_source_to_target(graphs[map_id], precursor,
+                                                                                            compound)
+                                    if path:
+                                        compound_prec_dict[compound] = list(
+                                            set(compound_prec_dict[compound]) | {precursor})
+                            except (nx.NodeNotFound, nx.NetworkXNoPath):
+                                pass
 
         return compound_prec_dict
 
@@ -111,22 +122,31 @@ class KEGGAllCompoundInfoExtractor:
 
         writer = Chem.SDWriter('output.sdf')
 
+        precursor_ids = set(precursor for precursors in compound_precursor_dict.values() for precursor in precursors)
+
         for compound, precursors in compound_precursor_dict.items():
             compound_structure = None
-            compound_map_ids = compound_map_dict.get(compound, [])
-
+            
             for map_id, graph in graphs.items():
                 if compound in graph.nodes:
                     compound_structure = graph.nodes[compound]["mol"]
                     break
 
             if compound_structure:
-
                 mol = Chem.MolFromMolBlock(compound_structure)
-                mol.SetProp("Precursors", ";".join(precursors))
-                mol.SetProp("Map_IDs", ";".join(compound_map_ids))
+
+                for precursor_id in precursor_ids:
+                    flag = "1" if precursor_id in precursors else "0"
+                    precursor_prop_name = precursor_id
+                    mol.SetProp(precursor_prop_name, flag)
+
+                mol.SetProp("Map_IDs", ";".join(compound_map_dict.get(compound, [])))
 
                 writer.write(mol)
+
+                if all(precursor_id == '0' for precursor_id in precursors):
+                    print(f"Compound {compound} has zero precursors")
+
             else:
                 print(f"Failed to retrieve structure for compound {compound}")
 
